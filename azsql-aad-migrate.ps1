17583af279cfbd82ff668cc61418fbb18162808e
Weixian <43234101+weixian-zhang@users.noreply.github.com>
2022-12-11 15:43:02 +0800

    refactor: Add filtering to ignore granting of permissions to users with permissions ['GRANT', 'CONNECT'']

--- azsql-aad-migrate.ps1
+++ azsql-aad-migrate.ps1
@@ -23,18 +23,28 @@ Function SetupPrequisites() {
 }
 
 Function Info($msg) {
-    Write-Host $msg -fore blue
+    $now = NowStr
+    Write-Host "[$now]: $msg" -fore blue
 }
 
+Function Warn($msg) {
+    $now = NowStr
+    Write-Host "[$now]: $msg" -fore Yellow
+}
+
+
 Function Success($msg) {
-    Write-Host $msg -fore green
+    $now = NowStr
+    Write-Host "[$now]: $msg" -fore green
 }
 
 Function Error($msg, $source) {
+    $now = NowStr
+    Write-Host "[$now]: ($source): $msg - {$_}" -fore red    
+}
 
-    Write-Host "($source): $msg - {$_}" -fore red
-
-    
+Function NowStr() {
+    return (get-date).ToLocalTime().ToString("dd-MM-yyyy HH:mm:ss")
 }
 
 Function IsNullOrEmptyStr($str) {
@@ -189,7 +199,7 @@ Function Process_Server_Logins_and_Mapped_DB_Users() {
 
     try {
 
-        Info "being recreating server-logins and DB users that mapped to server-logins"
+        Info "starting to recreate server-logins and DB users that mapped to server-logins"
 
         $dbUsersWithLogins = new-object System.Data.DataTable
         $dbUsersWithLogins.Columns.Add("database")
@@ -200,7 +210,11 @@ Function Process_Server_Logins_and_Mapped_DB_Users() {
         $dbUsersWithLogins.Columns.Add("SIDWithoutAADE")
         $dbUsersWithLogins.Columns.Add("serverLoginName")
         
+        Info "Retrieving databases"
+
         $dbs = Get_Databases
+
+        Info "databases retrieved"
 
         foreach($row in $dbs.Rows) {
 
@@ -222,14 +236,18 @@ Function Process_Server_Logins_and_Mapped_DB_Users() {
                     return      # return continue foreach loop
                 }
 
+                Info "DB user with SID {$sidWithoutAADE} finding matching server login"
+
                 $serverLoginDT = Find_Server_Logins_By_DB_User_SID $sidWithoutAADE
 
                 # this db user does have mapping to a server login
                 if ($serverLoginDT.Count -ne "0") {
-                    
+
                     foreach($row in $serverLoginDT.Rows) {
 
                         $serverLoginName = $row[0]
+
+                        Info "DB user with SID {$sidWithoutAADE} found matching server login {$serverLoginName}"
 
                         $newRow = $dbUsersWithLogins.NewRow()
                         $newRow.database = $dbName
@@ -256,42 +274,59 @@ Function Process_Server_Logins_and_Mapped_DB_Users() {
             $SIDWithoutAADE = $row[5]
             $serverLoginName = $row[6]
 
+            Info @"
+            being recreate server logins and remapping DB users with info:
+                database: $database
+                DB username: $username
+                DB user role: $role
+                DB user permission: $permission
+                sid: $sid
+                sid without AADE Suffix: $SIDWithoutAADE
+                Server Login Name: $serverLoginName
+"@
+
             if (Is_Server_Login_Exists $serverLoginName) {
+                
+                    Info "dropping server login {$serverLoginName} in master database"
 
                 Drop_Server_Login $serverLoginName
+
+                    Info "dropped server login {$serverLoginName} in master database"
 
                 Create_Server_Login $serverLoginName
 
                 if (Is_DB_User_Exist $database $username) {
 
-                        Info "DB user $userName exists in database $dbName, begin re-creating user and adding appropriate roles and permissions to users"
+                        Info "DB user $userName exists in database {$dbName}, begin re-creating user and adding appropriate roles and permissions to users"
 
                     Drop_DB_Contained_User $database $username
 
-                        Info "DB user $userName dropped in database $dbName"
+                        Info "DB user $userName dropped in database {$dbName}"
 
                     Create_DB_Users_FromServer_Login $database  $username $serverLoginName
 
-                        Info "DB user $userName created in database $dbName"
+                        Info "DB user $userName created in database {$dbName}"
 
                     if($role -ne "") {
-                            Info "Roles {$role} detected for DB user $userName database $dbName, adding role to user" 
+                            Info "Roles {$role} detected for DB user {$userName} in database {$dbName}, adding role to user" 
 
                         Alter_Role_DB_User $dbname $role $userName
                     }else {
-                        Info "No roles detected for DB user in $userName database $dbName"
+                        Info "No roles detected for DB user {$userName} in database {$dbName}"
                     }
                     
-                    if($permission -ne "") {
+                    if($permission -ne "" -And $permission -ne "GRANT" -And $permission -ne "CONNECT") {
                             Info "Permission {$permission} detected for DB user $userName  database $dbName, adding permission to user" 
 
                         Grant_DB_User_Permissions $dbname $permission $userName
                     }else {
-                        Info "No permission detected for DB user $userName database $dbName"
+                        Warn "Ignore granting following permissions for DB user {$userName} ['GRANT', 'CONNECT', {no permission}] in database {$dbName}"
                     }
                 }
             }            
         }
+
+        Success "successfully recreated server-logins and DB users mapped to server-logins"
     }
     catch {
         Error "" "Process_Server_Logins_and_Mapped_DB_Users"
@@ -300,7 +335,6 @@ Function Process_Server_Logins_and_Mapped_DB_Users() {
 
 Function Drop_Server_Login($loginName) {
     
-
     try {
 
         $conn = Create_SQL_Connection "master" $aad_sqladmin_username $aad_sqladmin_password
@@ -434,17 +468,6 @@ Function Recreate_Server_Logins() {
 
 # TODO
 Function Get_DB_Users_Roles_Permissions_With_Server_Logins($dbName) {
-#     $tsql = @"
-#     SELECT * FROM
-#     (
-#         SELECT name, type_desc, sid, 
-#         LEFT(CONVERT(NVARCHAR(1000), sid, 2), LEN(CONVERT(NVARCHAR(1000), sid, 2))- 4 ) as SIDWithoutAADE, 
-#         RIGHT(CONVERT(NVARCHAR(1000), sid, 2), 4) as AADExternalServerLogin
-#         FROM sys.database_principals 
-#         WHERE type_desc like 'external%'
-#     ) t
-#     WHERE t.AADExternalServerLogin = 'AADE'
-# "@
 
     $tsql = @"
     select 
@@ -541,10 +564,18 @@ Function Process_Database_Contained_Users() {
 
     try {
 
+        Info "being recreating db contained users"
+
+        Info "retrieving databases"
+
         $dbs = Get_Databases
+
+        Info "retrieve databases complete"
 
         foreach($row in $dbs.Rows) {
             $dbName = $row[0]
+
+            Info "retrieving db contained user users, roles and permissions 'with no match server-logins' "
 
             $dbUsers = Get_DB_Users_Roles_Permissions_Without_Server_Login($dbName)
 
@@ -580,17 +611,19 @@ Function Process_Database_Contained_Users() {
                             Info "No roles detected for DB user in $userName database $dbName"
                         }
                         
-                        if($permission -ne "") {
+                        if($permission -ne "" -And $permission -ne "GRANT" -And $permission -ne "CONNECT") {
                             Info "Permission {$permission} detected for DB user $userName  database $dbName, adding permission to user" 
 
                             Grant_DB_User_Permissions $dbname $permission $userName
                         }else {
-                            Info "No permission detected for DB user $userName database $dbName"
+                            Warn "Ignore granting following permissions for DB user {$userName} ['GRANT', 'CONNECT', {no permission}] in database {$dbName}"
                         }
                     }
                 }
             }
         }
+
+        Success "recreation of db contained users completed successfully"
     }
     catch {
         Error "error when processing db $userName, $errMsg in database $dbName" "Process_Database_Users"
@@ -733,7 +766,7 @@ SetupPrequisites
 
 Test-SQLConnection "master" $aad_sqladmin_username $aad_sqladmin_password
 
-Success "Azure AD SQL Admin $aad_sqladmin_username is able to connect to DB server $sql_server_name"
+Success "Azure AD SQL Admin $aad_sqladmin_username is able to connect to SQL Server $sql_server_name"
 
 Process_Database_Contained_Users
 
--- test/db-level-users.sql
+++ test/db-level-users.sql
@@ -42,25 +42,13 @@ select
         members.name not in ('dbo') and
         RIGHT(CONVERT(NVARCHAR(1000), members.sid, 2), 4) <> 'AADE'
 
--- SELECT    roles.principal_id                            AS RolePrincipalID
--- 	,    roles.name                                    AS RolePrincipalName
--- 	,    database_role_members.member_principal_id    AS MemberPrincipalID
--- 	,    members.name                                AS MemberPrincipalName
--- 	, perms.permission_name
--- 	, members.type_desc as MemberType
--- FROM sys.database_role_members AS database_role_members
--- JOIN sys.database_principals AS roles  
--- 	ON database_role_members.role_principal_id = roles.principal_id  
--- JOIN sys.database_principals AS members  
--- 	ON database_role_members.member_principal_id = members.principal_id
--- LEFT JOIN (SELECT
--- 		class_desc
--- 		, CASE WHEN class = 0 THEN DB_NAME()
--- 			    WHEN class = 1 THEN OBJECT_NAME(major_id)
--- 			    WHEN class = 3 THEN SCHEMA_NAME(major_id) END [Securable]
--- 		, USER_NAME(grantee_principal_id) [AADUser]
--- 		, permission_name
--- 		, state_desc
--- 		FROM sys.database_permissions) perms
--- 	ON perms.[AADUser] = members.name
--- WHERE members.type_desc like 'external%'
+
+    SELECT * FROM
+    (
+        SELECT name, type_desc, sid, 
+        LEFT(CONVERT(NVARCHAR(1000), sid, 2), LEN(CONVERT(NVARCHAR(1000), sid, 2))- 4 ) as SIDWithoutAADE, 
+        RIGHT(CONVERT(NVARCHAR(1000), sid, 2), 4) as AADExternalServerLogin
+        FROM sys.database_principals 
+        WHERE type_desc like 'external%'
+    ) t
+    WHERE t.AADExternalServerLogin = 'AADE'
